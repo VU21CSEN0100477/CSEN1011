38da8ac2aa3eb2a4561c1ae7819469305b5ea4b7
shivakumar477 <93367794+shivakumar477@users.noreply.github.com>
2021-11-02 14:17:40 +0530

    Add files via upload

---
+++ oddneven.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? x;
+
+   raptor_prompt_variable_zzyz ="Enter a number";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> x;
+   if (x % 2==0)
+   {
+      cout << "The number is even" << endl;   }
+   else
+   {
+      cout << "The number is odd" << endl;   }
+
+   return 0;
+}
