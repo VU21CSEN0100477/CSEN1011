c78396ee087e05898167f996452aad98b374dfb1
shivakumar477 <93367794+shivakumar477@users.noreply.github.com>
2021-12-21 14:24:09 +0530

    Add files via upload

---
+++ floyd.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int r,i,j,n=1;
+	printf("Enter the total number of rows:");
+	scanf("%d",&r);
+	for(i=1;i<=r;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%4d",n);
+			++n;
+		}
+		printf("\n");
+	}
+}
