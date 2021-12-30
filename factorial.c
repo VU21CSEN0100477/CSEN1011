d33594d47d514ff38500e52443cb39b487b21460
shivakumar477 <93367794+shivakumar477@users.noreply.github.com>
2021-12-30 14:35:54 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,28 @@
+/* Algorithm
+01. Start
+02. Accept the value and run it with the given formula
+03. Display the result obeying the given conditions
+04. Stop */
+
+#include<stdio.h>
+int main()
+{
+    int n,s=1;
+    scanf("%d",&n);
+    if(n<=9 && n>=0)
+    {
+    for(int i=1;i<=n;i++)
+    {
+        s=s*i;
+    }
+    printf("%d",s);
+    }
+    else if(n<0)
+    {
+        printf("Negative Number");
+    }
+    else
+    {
+         printf("Too big a number");
+    }
+}
